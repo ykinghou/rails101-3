a1d644fa3e53c46eff22b40ad599702651184e4a
yking,hou <hyking@hotmail.com>
2017-03-29 21:26:57 +0800

    use fimple_format helper to decorate

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,11 @@ Rails.application.routes.draw do
      end
      resources :posts
    end
+
+   namespace :account do
+     resources :groups
+     resources :posts
+   end
+
    root 'groups#index'
 end
---
+++ test/controllers/account/group_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Account::GroupControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
